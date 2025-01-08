e5ddb2e986a284597523e81d8bf13f28307263e9
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2025-01-08 23:27:48 +0900

    [feat]: ログイン認証実装

--- atc/src/login.rs
+++ atc/src/login.rs
@@ -1,7 +1,11 @@
-use reqwest::{Client, Error};
+use reqwest::cookie::Jar;
+use reqwest::Client;
 use rpassword::read_password;
 use scraper::{Html, Selector};
-use std::io::{self, Write};
+use std::{
+    io::{self, Write},
+    sync::Arc,
+};
 
 pub struct UserCredentials {
     pub user_id: String,
@@ -34,4 +38,55 @@ fn prompt_password(prompt: &str) -> Result<String, io::Error> {
     io::stdout().flush()?;
     let password = read_password()?;
     Ok(password)
+}
+
+pub async fn login_to_atcoder(
+    credentials: &UserCredentials,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let login_url = "https://atcoder.jp/login";
+
+    // Cookieストアを作成
+    let cookie_store = Arc::new(Jar::default());
+
+    // Cookieストアを持つClientを作成
+    let client = Client::builder()
+        .cookie_store(true)
+        .cookie_provider(Arc::clone(&cookie_store))
+        .build()?;
+
+    // csrf_tokenを取得
+    let selector = Selector::parse("input[name=\"csrf_token\"]").unwrap();
+    let body = client.get(login_url).send().await?.text().await?;
+    let document = Html::parse_document(&body);
+    let csrf_token = document
+        .select(&selector)
+        .next()
+        .unwrap()
+        .value()
+        .attr("value")
+        .unwrap()
+        .to_string();
+
+    // ログインデータの作成
+    let login_form = [
+        ("username", credentials.user_id.as_str()),
+        ("password", credentials.password.as_str()),
+        ("csrf_token", csrf_token.as_str()),
+    ];
+
+    // ログインリクエストの送信
+    let login_response = client.post(login_url).form(&login_form).send().await?;
+    login_response.error_for_status_ref()?;
+    let response_url = login_response.url().to_string();
+    if response_url == "https://atcoder.jp/home" {
+        println!("ログインに成功しました！");
+        Ok(())
+    } else {
+        println!("ログインに失敗しました:");
+        Err(format!(
+            "ログイン失敗: リダイレクト先が/homeではありません ({})",
+            response_url
+        )
+        .into())
+    }
 }
--- atc/src/main.rs
+++ atc/src/main.rs
@@ -1,16 +1,14 @@
 mod login;
+use tokio;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("ログイン処理を開始します...");
     match login::get_credentials() {
-        Ok(credentials) => {
-            // 入力結果の確認（デバッグ用、実運用では削除）
-            println!("ユーザーID: {}", credentials.user_id);
-            println!("パスワードは入力されました（表示されません）。");
-
-            // 次の処理（例: AtCoderへのログイン）を呼び出す
-            // login::login_to_atcoder(&credentials); // 次に実装
-        }
+        Ok(credentials) => match login::login_to_atcoder(&credentials).await {
+            Ok(_) => println!("AtCoderへのログインが完了しました！"),
+            Err(e) => eprintln!("ログイン中にエラーが発生しました: {}", e),
+        },
         Err(e) => {
             eprintln!("ログイン情報の取得に失敗しました: {}", e);
         }
