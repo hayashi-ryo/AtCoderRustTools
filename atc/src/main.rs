44b0ed300a9e9bd99ce11060c938b1095a98edff
hayashi-ryo <59258630+hayashi-ryo@users.noreply.github.com>
2025-01-11 00:57:28 +0900

    [feat]: サブコマンド対応中

--- atc/src/main.rs
+++ atc/src/main.rs
@@ -1,17 +1,38 @@
-mod login;
+mod commands;
+use clap::{Args, Parser, Subcommand};
 use tokio;
+
+#[derive(Parser)]
+#[command(name = "cargo-atc")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    login,
+    test,
+}
 
 #[tokio::main]
 async fn main() {
     println!("ログイン処理を開始します...");
     let login_url = "https://atcoder.jp";
-    match login::get_credentials() {
-        Ok(credentials) => match login::login_to_atcoder(&credentials, &login_url).await {
-            Ok(_) => println!("AtCoderへのログインが完了しました！"),
-            Err(e) => eprintln!("ログイン中にエラーが発生しました: {}", e),
-        },
+    match commands::login::get_credentials() {
+        Ok(credentials) => {
+            match commands::login::login_to_atcoder(&credentials, &login_url).await {
+                Ok(_) => println!("AtCoderへのログインが完了しました！"),
+                Err(e) => eprintln!("ログイン中にエラーが発生しました: {}", e),
+            }
+        }
         Err(e) => {
             eprintln!("ログイン情報の取得に失敗しました: {}", e);
         }
     }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
 }
